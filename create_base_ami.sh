ddcf4f2ba59f9331198a1cc1c9ea156270749142
Siebrand Mazeland <siebrand@kitano.nl>
2018-11-14 21:10:05 +0100

    Ignore dependencies installing the centos-release rpm
    Otherwise the script exits with:
    
    Failed dependencies:
    	/bin/sh is needed by centos-release-7-5.1804.el7.centos.x86_64
    	coreutils is needed by centos-release-7-5.1804.el7.centos.x86_64
    	grep is needed by centos-release-7-5.1804.el7.centos.x86_64

--- create_base_ami.sh
+++ create_base_ami.sh
@@ -26,7 +26,7 @@ mkdir -p "$ROOTFS"
 mount "$PARTITION" "$ROOTFS"
 
 rpm --root="$ROOTFS" --initdb
-rpm --root="$ROOTFS" -ivh \
+rpm --root="$ROOTFS" --nodeps -ivh \
   https://mirrors.edge.kernel.org/centos/7.5.1804/os/x86_64/Packages/centos-release-7-5.1804.el7.centos.x86_64.rpm
 yum --installroot="$ROOTFS" --nogpgcheck -y groupinstall core
 yum --installroot="$ROOTFS" --nogpgcheck -y install openssh-server grub2 tuned kernel chrony
