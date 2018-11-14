7abaf62b77ae7e04ba8ce7a792212af1c70df22a
Siebrand Mazeland <siebrand@kitano.nl>
2018-11-14 22:56:19 +0100

    Use Amazon Time Sync Service instead of internet sources for time

--- scripts/install_prereqs.sh
+++ scripts/install_prereqs.sh
@@ -50,6 +50,11 @@ yum -y install --tolerant perl tar xz unzip curl bind-utils net-tools ipset libt
 
 echo ">>> Installing things that Irving cares about"
 yum -y install lvm2 xfsprogs python-setuptools yum-utils git wget tuned sysstat iotop perf nc telnet vim awscli bash-completion lsof mlocate
+# Use Amazon Time Sync Service
+# See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/set-time.html for details
+# Remove superfluous server configurations
+sed -i '/^server [123]/d' chrony.conf
+sed -i 's/0.centos.pool.ntp.org/169.254.169.123 prefer/' chrony.conf
 # enable chronyd (better than NTP)
 systemctl enable chronyd.service
 
