ab3ecbb17aa7561fc22ad663e1176c0d502779a5
Siebrand Mazeland <siebrand@kitano.nl>
2018-11-15 01:06:43 +0100

    Disable "exit on error" during check
    A non-0 return value would be expected here.

--- scripts/install_ssm_agent.sh
+++ scripts/install_ssm_agent.sh
@@ -2,6 +2,9 @@
 set -o errexit -o nounset -o pipefail
 
 echo ">>> Installing latest Amazon SSM agent"
+# systemctl status will return a non-0 exit value if the service is not know,
+# so "error on exit" has to be disabled until after this check.
+set +e
 systemctl status amazon-ssm-agent
 if [ $? = 0 ]
 then
@@ -12,6 +15,7 @@ then
 else
     echo ">>>> Amazon SSM agent not yet installed"
 fi
+set -e
 yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 systemctl enable amazon-ssm-agent
 
